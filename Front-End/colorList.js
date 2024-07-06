8f5b0086f63f9f3e65562d960ec5ed9989d2cac9
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-07-06 09:35:05 +0330

    Ready-component: update Color.js pallet

--- Front-End/colorList.js
+++ Front-End/colorList.js
@@ -1,31 +1,38 @@
-export const FOREGROUND =  "#f8f8f2";
+export const FOREGROUND =  "#f8f8f8";
 export const BACKGROUND =  "#0a1045";
 export const MYBLUE =      "#0d6efd";
+export const blue =        "#007bff";
+export const MYBLUE2 =     "#3498db";
 export const LOGOCOLOR1 =  "#00c2d1";
 export const CYAN =        "#8be9fd";
 export const CURRENTLINE = "#44475a";
 export const COMMENT =     "#6272a4";
 
 export const YELLOW =      "#f9e900";
-export const YELLOW2 =     "#f1fa8c";
+export const YELLOW4 =     "#f4b932";
 export const YELLOW3 =     "#ffc44d";
+export const YELLOW2 =     "#f1fa8c";
 
 export const RED =         "#ff5555";
 export const GHAHVEI =     "#f6af65";
 
+export const PURPLE2 =     "#8e44ad";
 export const PURPLE =      "#bd93f9";
+
+export const JEEGARI2 =    "#EE2A7B";
+export const PINK2 =       "#ff1756";
+export const JEEGARI1 =    "#DA1C5C";
 export const SOORATI =     "#ed33b9";
 export const PINK =        "#ff79c6";
+
+export const GREEN2 =      "#1cb65d";
 export const GREEN =       "#50fa7b";
 
 export const LOGOCOLOR2 =  "#02BDB2";
-export const JEEGARI1 =    "#DA1C5C";
-export const JEEGARI2 =    "#EE2A7B";
 
 
-export const blue =        "#007bff";
-export const indigo =      "#6610f2";
-export const purple =      "#6f42c1";
+export const purple =      "#6610f2";
+export const purple2 =     "#6f42c1";
 export const pink =        "#e83e8c";
 export const red =         "#dc3545";
 export const orange =      "#fd7e14";
